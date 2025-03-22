43a2d3a8cd3b9b725d1ed0efe5ed3ae91790f61b
Stepan Lemtyugin <stepanlemtugin@gmail.com>
2025-03-22 23:16:04 +0500

    Add Timer for Models Tests

--- StepanLem/HighLevelAcces.py
+++ StepanLem/HighLevelAcces.py
@@ -1,5 +1,10 @@
 import whisper
+import time
+
+start_time = time.time()
 
 model = whisper.load_model("turbo")
 result = model.transcribe("StepanLem/lfs/TestVideo.mp4")
 print(result["text"])
+
+print("--- %s seconds ---" % (time.time() - start_time))
